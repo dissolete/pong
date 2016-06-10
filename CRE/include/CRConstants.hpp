215f0b86b28df885c808de7c7bc1da9b0852243b
dissolete <aequus.junk@yahoo.com>
2016-06-10 12:46:46 -0700

    CRE Core has been reoptimized. Better organization. Improved makefile. Project better staged to be worked on.

---
+++ CRE/include/CRConstants.hpp
@@ -0,0 +1,11 @@
+#ifndef CRCONSTANTS_HPP
+#define CRCONSTANTS_HPP
+
+namespace CRE
+{
+	const int STATUS_OK = 0;
+	const int STATUS_APP_INIT_FAILED = -1;
+	const int STATUS_APP_STACK_EMPTY = -2;
+}
+
+#endif
---
+++ CRE/include/CRTypes.hpp
@@ -0,0 +1,13 @@
+#ifndef CRTYPES_HPP
+#define CRTYPES_HPP
+
+#include <stdint.h>
+
+namespace CRE
+{
+	class App;
+	class State;
+	class StateManager;
+}
+
+#endif
